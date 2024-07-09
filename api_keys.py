21f8177b242ba94fb7e1ae824f356ac7082e36a9
nmincucci <nmincucci2@yahoo.com>
2024-07-09 19:13:40 -0400

    attempt 2

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "https://api.weatherapi.com/weather/48687f6ce8e14cc255612e63e47ade58"
+weather_api_key = "48687f6ce8e14cc255612e63e47ade58"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "https://myprojects.geoapify.com/api/0cfdb17286dc4e5aa91875aeb6f17f2c"
