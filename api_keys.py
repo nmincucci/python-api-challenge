0b20ba2c97b96dbb3d09971453b42af0a1f39404
nmincucci <nmincucci2@yahoo.com>
2024-07-09 19:09:17 -0400

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "https://api.weatherapi.com/weather/48687f6ce8e14cc255612e63e47ade58"
+weather_api_key = "1fc6884d64b1d9e4f941552c2e2b096d"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0cfdb17286dc4e5aa91875aeb6f17f2c"
